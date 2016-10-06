3a5aae2eef60ee2b9f3709285b73d9595a54fd22
Cesar Guerra <cesargb504@gmail.com>
2016-10-05 22:53:44 -0700

    cambio en configuracion de bd

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20160917001845) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -37,8 +34,8 @@ ActiveRecord::Schema.define(version: 20160917001845) do
     t.integer  "objective_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["objective_id"], name: "index_publications_on_objective_id", using: :btree
-    t.index ["user_id"], name: "index_publications_on_user_id", using: :btree
+    t.index ["objective_id"], name: "index_publications_on_objective_id"
+    t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,6 +47,4 @@ ActiveRecord::Schema.define(version: 20160917001845) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "publications", "objectives"
-  add_foreign_key "publications", "users"
 end
